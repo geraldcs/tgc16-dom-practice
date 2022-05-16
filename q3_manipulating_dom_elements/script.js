e445d453ce75f737fb8e458417f465048671c5ba
Fritz Latagan <76860993+geraldcs@users.noreply.github.com>
2022-05-16 19:08:48 +0800

    Update script.js

--- q3_manipulating_dom_elements/script.js
+++ q3_manipulating_dom_elements/script.js
@@ -1,7 +1,8 @@
 // USE THOSE VARIABLES TO MODIFY THE DOM ELEMENTS
-let header = document.querySelector('h1');
-let important = document.querySelector('#important');
-let todo = document.querySelector('li.todo');
+let header = document.querySelector('h1').innerText = "Hello world!".toLowerCase();
+let important = document.querySelector('span#important').style.backgroundColor = 'red';
+let todo = document.querySelector('li.todo').style.fontFamily = 'Verdana'.toLowerCase();
+todo = document.querySelector('li.todo').style.fontSize = '16px';
 
 // LEAVE THE BELOW ALONE! DON'T TOUCH
 try {
