b72dc44850ab2eb68bc1dd5728580db567a73a72
Fritz Latagan <76860993+geraldcs@users.noreply.github.com>
2022-05-19 17:12:45 +0800

    Update script.js

--- q5_event_handlers/script.js
+++ q5_event_handlers/script.js
@@ -1,3 +1,5 @@
 // PLACE YOUR CODE HERE
-
+let changeBtn = document.querySelector('#changeColor').addEventListener('click', () => {
+    document.querySelector('h1').style.backgroundColor = 'yellow';
+})
 
