a6e043bf2212623cf33efe7677babb52f8d6c992
Fritz Latagan <76860993+geraldcs@users.noreply.github.com>
2022-05-19 18:18:18 +0800

    Update script.js

--- q8_manipulating_dom_elements/script.js
+++ q8_manipulating_dom_elements/script.js
@@ -1,3 +1,22 @@
 // PLACE YOUR CODE HERE
 
+let emphasis = document.querySelectorAll('.emphasis');
+for (e of emphasis) {
+    e.style.fontFamily = 'Verdana';
+}
+
+let finished = document.querySelectorAll('li.finished');
+for (f of finished) {
+    f.style.textDecoration = 'line-through';
+}
+
+let todo = document.querySelectorAll('ul#low-priority li.todo');
+for (t of todo) {
+    t.style.backgroundColor = 'blue';
+}
+
+let alert = document.querySelectorAll('.alert');
+for (a of alert) {
+    a.style.color = 'red';
+}
 
