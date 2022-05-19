bf7a6ccf5e12b6147e99399e485f1bd897864089
Fritz Latagan <76860993+geraldcs@users.noreply.github.com>
2022-05-19 16:24:23 +0800

    Update script.js

---
+++ q4_more_manipulation/script.js
@@ -0,0 +1,24 @@
+let header = document.querySelector('h1');
+header.style.color = 'green';
+
+let finished = document.querySelector('li.finished');
+finished.innerText = "repay credit card debt";
+
+let urgent = document.querySelector('li#urgent');
+urgent.style.borderColor = 'black';
+urgent.style.borderWidth = '2px';
+
+let firstEmphasis = document.querySelector('.emphasis');
+firstEmphasis.style.textDecoration = 'underline';
+
+let emphasis = document.querySelector('p.emphasis');
+emphasis.style.backgroundColor = 'orange';
+emphasis.style.fontSize = '32px';
+emphasis.style.lineHeight = '24px';
+
+let todo = document.querySelector('li#urgent span.todo');
+todo.style.backgroundColor = 'yellow';
+
+let greetings = document.querySelector('h2');
+greetings.innerHTML = '<span class="greetings">About Us</span>';
+
